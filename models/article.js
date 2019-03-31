3a418e9c32d20b8c377fb27f252f54e0e435fe01
dudueasy <dudueasy@126.com>
2019-04-01 01:18:21 +0800

    对 model 使用配置项

--- models/article.js
+++ models/article.js
@@ -1,6 +1,11 @@
 // this module returns a mongoose Model for acfun articles storage
-const mongoose =  require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, "../.env")});
+
+const DB_COLLECTION = process.env.DB_COLLECTION;
 
 // build mongoose connection
 require("../services/mongoose_db_connection");
@@ -9,11 +14,11 @@ const ArticleSchema = new Schema({
   resourceId: String,
   content: Schema.Types.Mixed,
   articleContentHtml: String,
-  createdAt: { type: String, default: Date.now().valueOf() },
-  originalCreatedAt:String,
+  createdAt: {type: String, default: Date.now().valueOf()},
+  originalCreatedAt: String,
   title: {type: String, required: true},
   tags: [{name: String, value: String, score: Number}],
-})
+});
 
-const articleModel = mongoose.model('article',ArticleSchema)
-module.exports = articleModel
+const articleModel = mongoose.model(DB_COLLECTION, ArticleSchema);
+module.exports = articleModel;
